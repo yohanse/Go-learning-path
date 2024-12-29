b03a08b7301212a1a7bb460db515fe41497a052c
yohanse <mehabawyohanse793@gmail.com>
2024-12-29 16:34:01 +0300

    write helper function for hashing and checking the password

--- jwt-autherntication-tutorial-project/models/user_model.go
+++ jwt-autherntication-tutorial-project/models/user_model.go
@@ -1,8 +1,9 @@
 package models
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
-  )
+)
 
 type User struct {
 	gorm.Model
@@ -10,4 +11,21 @@ type User struct {
   	Username string `json:"username" gorm:"unique"`
   	Email    string `json:"email" gorm:"unique"`
   	Password string `json:"password"`
+}
+
+func (user *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+
+func (user *User) CheckPassword(password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
 }
