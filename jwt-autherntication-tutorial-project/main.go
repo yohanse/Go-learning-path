5a45af6eb1f86f6f773b4a1e8d73cb2525963b44
yohanse <mehabawyohanse793@gmail.com>
2024-12-29 16:31:27 +0300

    setting up the connection string

--- jwt-autherntication-tutorial-project/main.go
+++ jwt-autherntication-tutorial-project/main.go
@@ -3,5 +3,5 @@ package main
 import "example/jwt-authentication-tutorial-project/database"
 
 func main() {
-	database.Connect("root:root@tcp(localhost:3306)/jwt_demo?parseTime=true")
+	database.Connect("postgresql://postgres:postgres@localhost:5432/jwtautherntication")
 }
