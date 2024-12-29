4d0c54771490af84221bbd4fd477f40ece860e4b
yohanse <mehabawyohanse793@gmail.com>
2024-12-29 17:16:33 +0300

    implement middleware for secured apis

---
+++ jwt-autherntication-tutorial-project/middleware/authmiddleware.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"example/jwt-authentication-tutorial-project/controller"
+	"github.com/gin-gonic/gin"
+)
+func Auth() gin.HandlerFunc{
+  return func(context *gin.Context) {
+    tokenString := context.GetHeader("Authorization")
+    if tokenString == "" {
+      context.JSON(401, gin.H{"error": "request does not contain an access token"})
+      context.Abort()
+      return
+    }
+    err:= controller.ValidateToken(tokenString)
+    if err != nil {
+      context.JSON(401, gin.H{"error": err.Error()})
+      context.Abort()
+      return
+    }
+    context.Next()
+  }
+}
