8789460a8e5c7c0e00ed2b02f34961d88c37644c
yohanse <mehabawyohanse793@gmail.com>
2024-12-29 17:31:46 +0300

    implement routing

--- jwt-autherntication-tutorial-project/main.go
+++ jwt-autherntication-tutorial-project/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "example/jwt-authentication-tutorial-project/database"
+import (
+	"example/jwt-authentication-tutorial-project/controller"
+	"example/jwt-authentication-tutorial-project/database"
+	"example/jwt-authentication-tutorial-project/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	database.Connect("postgresql://postgres:postgres@localhost:5432/jwtautherntication")
+	router := initRouter()
+  	router.Run(":8080")
 }
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+	  api.POST("/token", controller.GenerateToken)
+	  api.POST("/user/register", controller.RegisterUser)
+	  secured := api.Group("/secured").Use(middleware.Auth())
+	  {
+		secured.GET("/ping", controller.Ping)
+	  }
+	}
+	return router
+  }
